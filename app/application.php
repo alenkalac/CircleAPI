a3488e4b126ad157206750ed82d46bbc98529110
alenkalac <alenkalac87@gmail.com>
2017-09-29 00:53:53 +0100

    Registered Database Service Provider

--- app/application.php
+++ app/application.php
@@ -5,7 +5,20 @@
     $app = new Silex\Application();
     $app['debug'] = true;
 
-    require_once "routes.php";
+    $app->register(new Silex\Provider\SessionServiceProvider());
+    
+    $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+        'db.options' 	=> [
+            'driver' 	=> 'pdo_mysql',
+            'host' 		=> DBHOST,
+            'dbname' 	=> DBNAME,
+            'user' 		=> DBUSER,
+            'password' 	=> DBPASS,
+            'charset' 	=> 'utf8',
+            'port'		=> '3307'
+        ]
+    ));
 
+    require_once "routes.php";
 
     $app->run();
---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php 
+	define("DBHOST", "localhost");
+	define("DBNAME", "CircleRef");
+	define("DBUSER", "root");
+	define("DBPASS", "");
+
+	define("TWIG_TEMPLATE_PATH", __DIR__ . '/../templates');
+?>
