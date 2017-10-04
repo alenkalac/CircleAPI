6a38d0a3210102928e39119a712c5e663c6fcdbb
alenkalac <alenkalac87@gmail.com>
2017-10-04 13:00:07 +0100

    static fields to hold connection details

--- java/CircleRefServer/src/main/java/RequestFetcher.java
+++ java/CircleRefServer/src/main/java/RequestFetcher.java
@@ -2,9 +2,6 @@ import com.google.gson.Gson;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 
@@ -13,19 +10,46 @@ public class RequestFetcher implements Runnable {
     private int lastID;
     private ProcessRequests pRequsts;
     private OkHttpClient client;
-    private JSONParser parser;
+
+    public static String token = "";
+    public static String userID = "";
+    public static String accountID = "";
 
     public RequestFetcher(ProcessRequests pRequsts) {
         System.out.println("STARTING FETCH SERVICE");
+        this.client = new OkHttpClient();
+
         this.pRequsts = pRequsts;
+
+        init();
         lastID = 0; //API CALL TO GET THE LAST PROCESSED ID.
 
-        this.client = new OkHttpClient();
+
     }
     private void sleep(int millis) {
         try {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void init() {
+
+        try {
+            Request request = new Request.Builder().url("http://localhost/session").build();
+            Response response = client.newCall(request).execute();
+            String data = response.body().string();
+
+            Session session = new Gson().fromJson(data, Session.class);
+
+            RequestFetcher.token = session.getToken();
+            RequestFetcher.userID = session.getUser();
+            RequestFetcher.accountID = session.getAccount();
+
+            System.out.println("SETTING TOKEN TO " + token);
+
+        }catch(IOException e) {
             e.printStackTrace();
         }
     }
