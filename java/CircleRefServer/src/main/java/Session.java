8204f606a8ba6ca3a83385cc02007e48dc5c9ce8
alenkalac <alenkalac87@gmail.com>
2017-10-04 12:59:27 +0100

    Session data class

---
+++ java/CircleRefServer/src/main/java/Session.java
@@ -0,0 +1,46 @@
+
+public class Session
+{
+    private String token;
+
+    private String account;
+
+    private String user;
+
+    public String getToken ()
+    {
+        return token;
+    }
+
+    public void setToken (String token)
+    {
+        this.token = token;
+    }
+
+    public String getAccount ()
+    {
+        return account;
+    }
+
+    public void setAccount (String account)
+    {
+        this.account = account;
+    }
+
+    public String getUser ()
+    {
+        return user;
+    }
+
+    public void setUser (String user)
+    {
+        this.user = user;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [token = "+token+", account = "+account+", user = "+user+"]";
+    }
+}
+	
